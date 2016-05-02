fcf0aa6b6c56f56bffc897bf094b42a201cbe086
Ralph Maamari <ralphpal@hotmail.com>
2016-05-02 17:37:07 -0400

    Added the main files
    Added the application files.

---
+++ todo.js
@@ -0,0 +1,22 @@
+/**
+ * Created by RM on 5/2/2016.
+ */
+
+function TodoCtrl($scope){
+
+    $scope.todos = [{text:'Learn AngularJS', done:false}, {text: 'Build an app', done:false}];
+
+    $scope.getTotalTodos = function(){
+        return $scope.todos.length;
+    };
+
+    $scope.addTodo = function(){
+        $scope.todos.push({text:$scope.formTodoText, done:false});
+        $scope.formTodoText = "";
+    };
+
+    $scope.subtract = function(todo){
+        $scope.todos.pop();
+      return console.log("cows")
+    };
+}
